612133f85b16c4a58d14c4fc1e63a670f922bf6d
luthulim <luthuli_mahlasela@yahoo.com>
2026-05-04 22:07:55 +0200

    Added code to enable user to see if they can login successfully

--- src/test/java/stepsDef.java
+++ src/test/java/stepsDef.java
@@ -1,4 +1,5 @@
 import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,24 +10,31 @@ public class stepsDef {
     @Given("i am on the login page")
     public void i_am_on_the_login_page() {
 
-        driver= new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://ndosisimplifiedautomation.vercel.app/");
+        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/button/span[2]")).click();
 
     }
+
     @And("I enter email email")
     public void i_enter_email_email() {
+        driver.findElement(By.id("login-email")).sendKeys("admin@gmail.com");
 
     }
+
     @And("I enter password password")
     public void i_enter_password_password() {
-
+        driver.findElement(By.id("login-password")).sendKeys("@12345678");
     }
+
     @When("I click login button")
     public void i_click_login_button() {
-
+        driver.findElement(By.id("login-submit")).click();
     }
+
     @Then("i should be logged in successfully")
     public void i_should_be_logged_in_successfully() {
+        assert driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[4]/button/span[2]")).isEnabled();
 
     }
 
