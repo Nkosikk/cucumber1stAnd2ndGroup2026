c2fc82c6d5da94df56fd1404955f48805f1a5480
Morroco-Zungu <48709261+Morroco-Zungu@users.noreply.github.com>
2026-05-05 19:30:55 +0200

    added code for And, When and Then

--- src/test/java/stepsDef.java
+++ src/test/java/stepsDef.java
@@ -1,4 +1,5 @@
 import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,25 +13,32 @@ public class stepsDef {
         driver= new ChromeDriver();
         driver.get("https://ndosisimplifiedautomation.vercel.app/");
         driver.manage().window().maximize();
-        driver.quit();
 
     }
     @And("I enter email email")
     public void i_enter_email_email() {
-
+        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/button/span[2]")).click();
+        driver.findElement(By.id("login-email")).sendKeys("zakes@gmail.com");
 
     }
     @And("I enter password password")
     public void i_enter_password_password() {
-
+        driver.findElement(By.id("login-password")).sendKeys("@12345678");
     }
     @When("I click login button")
     public void i_click_login_button() {
+        driver.findElement(By.id("login-submit")).click();
 
     }
     @Then("i should be logged in successfully")
     public void i_should_be_logged_in_successfully() {
-
+        driver.findElement(By.xpath("//*[@id=\"app-main-content\"]/section/div[1]/h2")).isDisplayed();
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
     }
 
 }
