7e26bc69755569f896b0da8574cdb7bf14cbf614
Nkosi <njceles@gmail.com>
2026-05-13 20:05:50 +0200

    called the method to verify login is displayed

--- src/test/java/Steps/stepsDef.java
+++ src/test/java/Steps/stepsDef.java
@@ -9,6 +9,7 @@ public class stepsDef extends Base{
     @Given("i am on the login page")
     public void i_am_on_the_login_page() {
         homePage.clickLoginButton();
+        loginPage.verifyLoginPageIsDisplayed();
     }
 
     @And("I enter email (.*)$")
