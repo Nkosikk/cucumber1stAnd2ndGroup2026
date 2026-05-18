b6da21862e13625607c836337ebd061d2df1971c
sintutu <160270685+sintutu@users.noreply.github.com>
2026-05-18 19:42:25 +0200

    Extend steps and bindings to end of assignment

--- src/test/java/Steps/stepsDef.java
+++ src/test/java/Steps/stepsDef.java
@@ -32,12 +32,62 @@ public class stepsDef extends Base{
         dashboardPage.verifyDashboardPageIsDisplayed();
     }
 
-    @And("I click mobile automation")
-    public void iClickMobileAutomation() {
-
-    }
 
     @After
     public void closeBrowser() {
+    }
+
+    @And("I click on the logged in user")
+    public void iClickOnTheLoggedInUser() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("I click on the admin panel")
+    public void iClickOnTheAdminPanel() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @And("I click on the groups tab")
+    public void iClickOnTheGroupsTab() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @And("I click on the create group button")
+    public void iClickOnTheCreateGroupButton() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @And("I enter group name (.*)$")
+    public void iEnterGroupNameGroupName(String groupName) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @And("I enter group description (.*)$")
+    public void iEnterGroupDescriptionGroupDescription(String groupDescription) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @And("I enter year (.*)$")
+    public void iEnterYear(int year) { }
+
+    @And("I enter max capacity (.*)$")
+    public void iEnterMaxCapacityMaxCapacity(int maxCapacity) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @And("I enter start date (.*)$")
+    public void iEnterStartDateStartDate(String startDate) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @And("I enter end date (.*)$")
+    public void iEnterEndDateEndDate(String endDate) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("i should see the group created successfully")
+    public void iShouldSeeTheGroupCreatedSuccessfully() {
+        // Write code here that turns the phrase above into concrete actions
     }
 }
