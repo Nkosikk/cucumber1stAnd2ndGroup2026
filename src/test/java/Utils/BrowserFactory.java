b012ba6ad8877133b1acec5a88e5443ec495c3ac
AvelaNoganta <avisnamsisi@gmail.com>
2026-05-27 18:49:25 +0200

    Running headless

--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -15,7 +15,7 @@ public class BrowserFactory {
         EdgeOptions edgeOptions = new EdgeOptions();
 
         if (browserChoice.equalsIgnoreCase("chrome")) {
-            //chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--headless");
             driver = new ChromeDriver(chromeOptions);
         } else if (browserChoice.equalsIgnoreCase("edge")) {
             edgeOptions.addArguments("--headless");
